870f659e20196aa79f808b244e7d3b261e6e32f7
Rahul Suryawanshi <rahulsu@cybage.com>
2021-03-21 14:11:28 +0530

    nav-bar implementation

---
+++ src/containers/App/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './App.css';
+import Home from '../Home/Home';
+import Login from '../Login/Login';
+import Signup from '../Signup/Signup';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+import { LinkContainer } from 'react-router-bootstrap';
+import { BrowserRouter, Route }  from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        {/* Navbar */}
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand>Login Form Demo</Navbar.Brand>
+          <Nav className="ml-auto">
+            <LinkContainer to="/">
+              <Button className="nav-btn" variant="outline-primary">Home</Button>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <Button className="nav-btn" variant="outline-primary">Login</Button>
+            </LinkContainer>
+            <LinkContainer to="/signup">
+              <Button className="nav-btn" variant="outline-primary">Signup</Button>
+            </LinkContainer>
+          </Nav>
+        </Navbar>
+
+        {/* Body */}
+        <Route path="/" exact component={Home} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/signup" exact component={Signup} />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/Login/Login.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import './Login.css';
+
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isPasswordValid: false,
+      passwordEntered: false
+    }
+  }
+
+  passwordChangeHandler = (event) => {
+    const password = event.target.value;
+
+    this.setState({
+      passwordEntered: true
+    });
+
+    if (password.length < 5 || password.length > 10) {
+      this.setState({
+        isPasswordValid: false
+      });
+    } else {
+      this.setState({
+        isPasswordValid: true
+      });
+    }
+  }
+
+  render() {
+    let passwordBoxStyle = null;
+    if (this.state.passwordEntered && !this.state.isPasswordValid) {
+      passwordBoxStyle = {
+        boxShadow: '0 0 2px 1px rgba(255, 0, 0, 1)',
+      };
+    }
+
+    return (
+      <Container className="Login">
+        <Row>
+          <Col md={{ span: 8, offset: 2 }}>
+            <Card className="login-form-container">
+              <Form>
+                <Form.Group as={Row} controlId="formPlaintextEmail">
+                  <Form.Label column sm="2">
+                    Email
+                  </Form.Label>
+                  <Col sm="10">
+                    <Form.Control type="email" placeholder="Enter email" />
+                  </Col>
+                </Form.Group>
+
+                <Form.Group as={Row} controlId="formPlaintextPassword">
+                  <Form.Label column sm="2">
+                    Password
+                  </Form.Label>
+                  <Col sm="10">
+                    <Form.Control type="password" placeholder="Password" className="password-box" style={passwordBoxStyle} onChange={this.passwordChangeHandler} />
+                  </Col>
+                </Form.Group>
+              </Form>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Login;
